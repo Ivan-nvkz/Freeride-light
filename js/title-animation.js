0232a4fc590be12e305dd53b941297d4c095c1f0
Ivan-nvkz <66069539+Ivan-nvkz@users.noreply.github.com>
2022-10-04 19:29:16 +0700

    Add files via upload

---
+++ js/title-animation.js
@@ -0,0 +1,42 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+
+
+   // Плавная подгрузка контента  на странице  от MaxGraf start ==========================================
+
+   // const scrollItems = document.querySelectorAll('.scroll-item');
+
+   // const scrollAnimation = () => {
+   //    let windowCenter = (window.innerHeight / .1) + window.scrollY;
+   //    // console.log(windowCenter);
+   //    scrollItems.forEach(el => {
+   //       let scrollOffset = el.offsetTop + (el.offsetHeight / .1);
+   //       console.log(scrollOffset);
+   //       if (windowCenter >= scrollOffset) {
+   //          el.classList.add('animation-class');
+   //       } else {
+   //          // el.classList.remove('animation-class');
+   //       }
+   //    });
+   // };
+
+   // scrollAnimation();
+   // window.addEventListener('scroll', () => {
+   //    scrollAnimation();
+   // });
+
+   // Плавная подгрузка контента  на странице  от MaxGraf end  ==========================================
+
+
+
+
+
+
+
+
+});
+
+
